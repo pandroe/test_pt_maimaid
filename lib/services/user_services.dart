14ce0ee3070d2c89103a46dd829f90d9e9091290
pandroe <daffapandu2304@gmail.com>
2024-05-20 13:20:19 +0700

    create add user list screen

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -32,4 +32,22 @@ class UserService {
       throw Exception('Error deleting user: $e');
     }
   }
+
+  // Post or Create User
+  Future<Map<String, dynamic>> createUser(String name, String job) async {
+    final response = await http.post(Uri.parse('$apiUser'), body: {
+      'name': name,
+      'job': job,
+    });
+
+    if (response.statusCode == 201) {
+      // Jika permintaan berhasil
+      print('User created successfully: ${json.decode(response.body)}');
+      return json.decode(response.body);
+    } else {
+      // Jika permintaan gagal
+      print('Failed to create user. Status code: ${response.statusCode}');
+      throw Exception('Failed to create user');
+    }
+  }
 }
